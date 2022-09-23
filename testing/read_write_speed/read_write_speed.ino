6754ddda86f1fb68691023d7057529ba23a00a65
Jonas <jonas-focke@mailbox.org>
2022-09-23 21:47:35 +0200

    refactor: 🚀 remove unused tests

---
+++ testing/read_write_speed/read_write_speed.ino
@@ -0,0 +1,33 @@
+#define BYTEARRAY_SIZE 16
+
+
+byte testbyte;
+
+unsigned int debug_timestamp, index = 0;
+
+void setup() {
+	Serial.begin(2000000);
+
+ // memset(testbyte, 0x01000001, BYTEARRAY_SIZE / 8);
+
+ // for ( int i = 0; i < BYTEARRAY_SIZE - 1; i++ ) {
+ //  testbyte[i] = 0x41;
+ // }
+
+ testbyte = 0x42;
+
+  debug_timestamp = millis(); 
+}
+
+
+void loop() {
+  Serial.print("B");
+  index++;
+  if ( index > 10000 ) {
+    Serial.print("\n");
+    Serial.print(millis() - debug_timestamp);  
+    Serial.print(": ");
+    Serial.println(index);  
+    delay(20000);
+  }
+}
