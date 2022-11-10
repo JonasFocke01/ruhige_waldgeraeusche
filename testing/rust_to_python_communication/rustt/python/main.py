c5ee1c3ad83cab3fbe45677e381b01665f94b5db
= <you@example.com>
2022-11-10 19:48:52 +0100

    test: rust to python communication worked out

--- testing/rust_to_python_communication/rustt/python/main.py
+++ testing/rust_to_python_communication/rustt/python/main.py
@@ -1,10 +1,54 @@
 import time as t
-import fileinput
+from rpi_ws281x import *
+import json
+import sys
 
+print(sys.argv)
+
+config_file_path = ""
+
+if (len(sys.argv) > 1):
+    config_file_path = "../config.json"
+else:
+    config_file_path = "config.json"
+
+with open(config_file_path) as json_data_file:
+    config = json.load(json_data_file)
+
+LED_COUNT_PER_STRIP = config["leds"]["led_count_per_strip"]
+STRIP_COUNT         = config["leds"]["strip_count"]
+LED_COUNT           = LED_COUNT_PER_STRIP * STRIP_COUNT
+LED_PIN             = config["leds"]["led_pin"]
+LED_FREQ_HZ         = config["leds"]["led_freq_hz"]
+LED_DMA             = config["leds"]["led_dma"]
+LED_BRIGHTNESS      = config["leds"]["led_brightness"]
+LED_INVERT          = config["leds"]["led_invert"]
+LED_CHANNEL         = config["leds"]["led_channel"]
+PIXEL_OFFSET        = config["leds"]["pixel_offset"]
+FRAME_TIMING        = config["leds"]["frame_timing"]
+
+physical_strip      = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ,LED_DMA,LED_INVERT,LED_BRIGHTNESS,LED_CHANNEL)
+
+strips = []
+
+physical_strip.begin()
 
 while 1:
-    f = open("test.txt", "a")
-    f.write(input())
-    fileinput.input()
-    f.close()
-    t.sleep(3)
+    try:
+        std_input = input()
+        start = t.time()
+        std_input = std_input.split(" ")
+        print(std_input)
+        assert len(std_input) == LED_COUNT * 3
+        for strip_i in range(STRIP_COUNT):
+            for pixel_i in range(LED_COUNT_PER_STRIP):
+                physical_strip.setPixelColor(pixel_i , Color(int(std_input[strip_i * pixel_i]), int(std_input[strip_i * pixel_i + 1]), int(std_input[strip_i * pixel_i + 2])))
+        physical_strip.show()
+        print(t.time() - start)
+    except AssertionError:
+        print("Wrong input size!")
+    except KeyboardInterrupt:
+        print("See you soon!")
+        exit(0)
+    except:
+        print("Unknown exception occured")
--- testing/rust_to_python_communication/rustt/src/main.rs
+++ testing/rust_to_python_communication/rustt/src/main.rs
@@ -1,17 +1,41 @@
-use std::io::{self, Write};
+use std::io::{self, Write, BufRead};
 use std::process::{Command, Stdio};
 use std::{thread, time};
+use rand::Rng;
+use std::fs;
 
 fn main() {
+    let stdin = io::stdin();
+
+    let file = fs::File::open("config.json")
+                .expect("file should open read only");
+    
+    let json: serde_json::Value = serde_json::from_reader(file)
+                .expect("file should be proper JSON");
+
+    let leds = json.get("leds")
+                .expect("file should have leds key");
+
+    let led_pin = &leds["led_pin"];
+
+    print!("{:?}", led_pin);
+
     let mut child = Command::new("python3")
         .arg("python/main.py")
         .stdin(Stdio::piped())
-        .spawn()?;
+        .spawn()
+        .unwrap();
 
     let child_stdin = child.stdin.as_mut().unwrap();
-    
+    let mut rng = rand::thread_rng();
     loop {
-        child_stdin.write_all("Hello, world!\n")?;
+        let line1 = stdin.lock().lines().next().unwrap().unwrap();
+        let r = rng.gen_range(0..200);
+        let g = rng.gen_range(0..200);
+        let b = rng.gen_range(0..200);
+        println!("{} {} {}\n", r, g, b);
+        println!("{}", line1);
+        child_stdin.write_all(format!("{} {} {}\n", line1, 0, 0).as_bytes());
         thread::sleep(time::Duration::from_millis(1000));
     }
     
