6959ccda09d2da86b0196da98b3e2988bcc1010f
= <jonas-focke@mailbox.org>
2022-11-11 07:57:04 +0100

    feat: reading and writing threads refactored to objects

--- code/logic/src/main.rs
+++ code/logic/src/main.rs
@@ -1,91 +1,31 @@
 use std::time::Instant;
-use std::thread;
 
-use std::io::{self, BufRead};
-use rand::Rng;
-use std::fs;
+use input_reader::InputReader;
+pub mod input_reader;
+use led_renderer::LedRenderer;
+pub mod led_renderer;
 
-pub mod serial;
-pub mod write_leds;
-
-fn calculate_dmx(button_states: [u8; BUTTON_COUNT], debug: bool) -> [u8; 512] {
-    let channel_states: [u8; 512] = [0; 512];
-
-    if debug {
-        assert!(false, "Not implemented!");
-    }
-
-    // assert!(false, "Not implemented!{}\n", button_states[0]);
-    channel_states
-}
-
-fn calculate_leds(button_states: [u8; BUTTON_COUNT], led_strip_count: u8, debug: bool) -> [[[u8; 3]; 150]; led_strip_count] {
-    let pixel_states: [[[u8; 3]; 150]; led_strip_count] = [[[0; 3]; 150]; led_strip_count];
-
-    if debug {
-        assert!(false, "Not implemented!");
-    }
-    // assert!(false, "Not implemented!{}\n", button_states[0]);
-    
-
-
-    pixel_states
-}
-
-
+const FRAME_TIMING: u128 = 34;
 
 fn main() {
     print!("Startup\n");
 
-    let BUTTON_COUNT: u8 = 20;
-
-    //? configuration
-    let file = fs::File::open("config.json")
-                .expect("file should open read only");
-    let json: serde_json::Value = serde_json::from_reader(file)
-                .expect("file should be proper JSON");
-    let leds = json.get("leds")
-                .expect("file should have leds key");
-
-    let frame_timing = &leds["frame_timing"];
-    let led_strip_count = &leds["led_strip_count"];
-
     //? setup
 
-    //* the button states array where 0 represents NOT ACTIVE, 255 represents ACTIVE and 0-255 represent POTENTIOMETER VALUES
-    let mut button_states: [u8; BUTTON_COUNT] = [0; BUTTON_COUNT];
+    let input_reader = InputReader::new();
+    let led_renderer = LedRenderer::new();
+    // let dmx_renderer = DmxRenderer::new();
 
-    let mut serial_writing_thread;
-    // let mut serial_reading_thread;
+    //? infinite programmloop whose speed is capped by the FRAME_TIMING attribute
 
-    let mut dmx_values: [u8; 512] = [0; 512];
-    let mut dmx_calculating_thread;
-
-    let mut led_values: [[[u8; 3]; 150]; led_strip_count] = [[[0; 3]; 150]; led_strip_count];
-    let mut led_calculating_thread;
-
-    let mut keyboard_input_device = "/dev/ttyUSB0";
-
-    //? infinite programmloop
-    print!("Entering programmloop\n");
     loop {
         let fps_limit_timestamp = Instant::now();
         
-        serial_writing_thread  = thread::Builder::new().name(String::from("led writing")).spawn(move || write_leds::write_leds(false));
-        //serial_reading_thread  = thread::Builder::new().name(String::from("serial reading")).spawn(move || serial::read_serial(false));
-        led_calculating_thread = thread::Builder::new().name(String::from("led calculation")).spawn(move || calculate_leds(button_states, led_strip_count, false));
-        dmx_calculating_thread = thread::Builder::new().name(String::from("dmx calculation")).spawn(move || calculate_dmx(button_states, false));
-        
-        //let button_states_buffer = serial_reading_thread.unwrap().join().unwrap();
-        // if button_states_buffer.0 {
-        //     button_states = button_states_buffer.1;
-        // }
-        dmx_values = dmx_calculating_thread.unwrap().join().unwrap();
-        led_values = led_calculating_thread.unwrap().join().unwrap();
-        let _dummy_value = serial_writing_thread.unwrap().join();
+        input_reader.process_input("Serial");
+        led_renderer.render();
+        // dmx_renderer.render();
 
-        //wait until 33 ms passed to limit to 30 fps
         print!("{}\n", fps_limit_timestamp.elapsed().as_millis());
-        while fps_limit_timestamp.elapsed().as_millis() < frame_timing {}
+        while fps_limit_timestamp.elapsed().as_millis() < FRAME_TIMING {}
     }
 }
