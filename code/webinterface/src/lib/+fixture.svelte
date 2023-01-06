8a0b8d281523d6bdd43d57694ba6ff4db5ff45d5
Jonas <jonas-focke@mailbox.org>
2023-01-06 18:02:36 +0100

    feat: 🎨 add fixture component

---
+++ code/webinterface/src/lib/+fixture.svelte
@@ -0,0 +1,1 @@
+<img class="w-20 h-20" src="VictoryScan.jpg" alt="VictoryScan"/>
--- code/webinterface/src/routes/+page.svelte
+++ code/webinterface/src/routes/+page.svelte
@@ -1,2 +1,7 @@
 <h1>Welcome to SvelteKit</h1>
+<script lang="ts">
+    import Fixture from '$lib/+fixture.svelte';
+</script>
+
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Fixture />
